94a70e546dacb361991a6328a39d7b2243228afb
SteveAsterAfovo <steveasterafovo@gmail.com>
2025-11-18 05:44:03 +0100

    6

--- playground/layouts/default.vue
+++ playground/layouts/default.vue
@@ -3,6 +3,8 @@
     <!-- Header -->
     <AppHeader />
 
+     <SiteNotificationManager />
+
     <!-- Content -->
     <main class="flex-1 overflow-y-auto">
       <div class="px-4 py-6 sm:px-6 lg:px-8 sm:py-8">
